7d457047ee301399d2753ebb57a922ba35b5227b
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2024-02-19 22:29:44 +0100

    Add Habit Tracker

---
+++ HabitLogger/HabitLogger/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Microsoft.Data.Sqlite;
+
+namespace habit_tracker
+{
+    class Program
+    {
+        static string connectionString = @"Data Source=habit-Tracker.db";
+        static void Main(string[] args)
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText =
+                    @"CREATE TABLE IF NOT EXISTS making_money (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Date TEXT,
+                        Quantity INTEGER
+                        )";
+
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+
+            GetUserInput();
+        }
+
+        static void GetUserInput()
+        {
+            Console.Clear();
+            bool closeApp = false;
+            while (closeApp == false)
+            {
+                Console.WriteLine("\n\nMAIN MENU");
+                Console.WriteLine("\nWhat would you like to do?");
+                Console.WriteLine("\nType 0 to Close Application.");
+                Console.WriteLine("Type 1 to View All Records.");
+                Console.WriteLine("Type 2 to Insert Record.");
+                Console.WriteLine("Type 3 to Delete Record.");
+                Console.WriteLine("Type 4 to Update Record.");
+                Console.WriteLine("------------------------------------------\n");
+
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "0":
+                        Console.WriteLine("\nGoodbye!\n");
+                        closeApp = true;
+                        Environment.Exit(0);
+                        break;
+                    // case "1":
+                    //     GetAllRecords();
+                    //     break;
+                    case "2":
+                        Insert();
+                        break;
+                    // case "3":
+                    //     Delete();
+                    //     break;
+                    // case "4":
+                    //     Update();
+                    //     break;
+                    default:
+                        Console.WriteLine("\nInvalid Command. Please type a number from 0 to 4.\n");
+                        break;
+                }
+            }
+
+        }
+
+        private static void Insert()
+        {   
+            string date = GetDateInput();
+
+            int quant = GetNumberInput("\n Enter the quantity that you want to input.");
+
+            using(var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText = 
+                    $"INSERT INTO making_money(date, quantity) VALUES('{date}', {quant})";
+
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+
+        internal static string GetDateInput()
+        {
+            Console.WriteLine("Please insert the date: (Format: dd-mm-yy). 0 to return to the main menu.");
+            // Add format checking here
+            string input = Console.ReadLine();
+
+            if (input == "0") GetUserInput();
+
+            return input;
+        }
+
+        internal static int GetNumberInput(string message){
+            // Add format checking here
+            Console.WriteLine($"\n{message}\n");
+            int input = int.Parse(Console.ReadLine());
+
+            if (input == 0) GetUserInput(); 
+
+            return input;
+        }
+
+        private static void GetAllRecords()
+        {
+
+        }
+
+
+    }
+}
