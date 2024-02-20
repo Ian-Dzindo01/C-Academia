dc0fdfd913c5961161b30fea8d584a62f72405d6
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2024-02-20 20:36:48 +0100

    Add CodingTracker

---
+++ CodingTracker/CodingTracker/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Configuration;
+
+var name = ConfigurationManager.AppSettings["name"];
+var surname = ConfigurationManager.AppSettings["surname"];
+
+
+Console.WriteLine($"Hi my name is {name} {surname}");
